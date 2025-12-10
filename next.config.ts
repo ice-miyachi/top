182c29b1dc2a39a0c1af438d653b8c7827b6dfae
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 00:30:30 +0900

    next.config.ts を更新

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,23 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors *; frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://*.notion.site https://*.notion.so; child-src 'self' https://*.notion.site https://*.notion.so;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
