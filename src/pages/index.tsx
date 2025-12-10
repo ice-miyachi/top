e0f7e062c79e1208f436b6b220e83121da967651
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 07:38:59 +0900

    index.tsx を更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import HomePage from "./ice_miyachi"; // ← これが原因！
-export default HomePage;               // ← これも不要！
+import HomePage from "./ice_miyachi";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+export default HomePage;
