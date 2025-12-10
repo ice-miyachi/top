171112aa01a6da43c468603ff0c900333583cb51
ice-miyachi <icemiyachi@gmail.com>
2025-12-10 22:41:01 +0900

    taiseis_footprint.tsx を作成

---
+++ src/pages/ice_miyachi/taiseis_footprint.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function TaiseisFootpront() {
+  return (
+    <div
+      style={{
+        margin: 0,
+        background: "#ffffff",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        className="frame-wrap"
+        style={{
+          transform: "scale(0.5)",
+          transformOrigin: "top center",
+          width: "100%",
+        }}
+      >
+        {/* Notion埋め込み */}
+        <iframe
+          src="https://www.notion.so/2b7c89ce71ed8093a67ed3
