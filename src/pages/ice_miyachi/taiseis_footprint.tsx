a54a7ce1ef76e5b568fd4de551ec1d220cbc25d9
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 01:16:46 +0900

    taiseis_footprint.tsx を更新

--- src/pages/ice_miyachi/taiseis_footprint.tsx
+++ src/pages/ice_miyachi/taiseis_footprint.tsx
@@ -1,11 +1,11 @@
+// src/pages/ice_miyachi/taiseis_footprint.tsx
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Page() {
+export default function TaiseisFootprint() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // 画面幅でスマホ判定
     setIsMobile(window.innerWidth <= 600);
   }, []);
 
@@ -21,32 +21,22 @@ export default function Page() {
           }
         />
       </Head>
-      {/* ページ本体 */}
+
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        <iframe
+          src="https://dynamic-noodle-39f.notion.site/ebd//2b7c89ce71ed8093a67ed3df59c57eb5"
+          width="100%"
+          height="600"
+          frameBorder={0}
+          allowFullScreen
+        />
+      </div>
     </>
-  );
-}
-
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-import React from "react";
-
-export default function TaiseisFootprint() {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        overflow: "hidden",
-      }}
-    >
-     <iframe
-  src="https://dynamic-noodle-39f.notion.site/ebd//2b7c89ce71ed8093a67ed3df59c57eb5"
-  width="100%"
-  height="600"
-  frameBorder={0}
-  allowFullScreen
-/>
-    </div>
   );
 }
