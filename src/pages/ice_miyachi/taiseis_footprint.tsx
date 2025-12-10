980c509da59d4980b6ce48aa850b5654f259b319
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 01:02:19 +0900

    taiseis_footprint.tsx を更新

--- src/pages/ice_miyachi/taiseis_footprint.tsx
+++ src/pages/ice_miyachi/taiseis_footprint.tsx
@@ -1,3 +1,14 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="scale-50">
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
 import React from "react";
 
 export default function TaiseisFootprint() {
