e5b19973188725870ce6ef8cbeae194f1814551e
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 09:29:11 +0900

    taiseis_footprint.tsx を更新

--- src/pages/ice_miyachi/taiseis_footprint.tsx
+++ src/pages/ice_miyachi/taiseis_footprint.tsx
@@ -24,8 +24,10 @@ export default function TaiseisFootprint() {
 
       <div
         style={{
-          width: "100%",
-          height: "100dvh", // ← ここ変更
+          margin: 0,
+          padding: 0,
+          width: "100vw",
+          height: "100vh", 
           overflow: "hidden",
         }}
       >
@@ -34,7 +36,6 @@ export default function TaiseisFootprint() {
           width="100%"
           height="100%"
           style={{ border: "none" }}
-          allowFullScreen
         />
       </div>
     </>
