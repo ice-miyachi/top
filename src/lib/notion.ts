6de79334118489ce3cc4042e45fa9adababddb2b
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 18:50:43 +0900

    notion.ts を更新

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,6 +1,7 @@
 // lib/notion.ts
 import { Client } from "@notionhq/client";
 
+// Notion クライアントを作成
 export const notion = new Client({
-  auth: process.env.NOTION_TOKEN, // ← Vercelに追加したトークンを使う
+  auth: process.env.NOTION_TOKEN, // ← 環境変数に設定したトークンを参照
 });
