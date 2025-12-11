79af0c1033fd9e29ae62ab45706bfdeff1f2238e
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 18:16:42 +0900

    taiseis_footprint.tsx を更新

--- src/pages/ice_miyachi/taiseis_footprint.tsx
+++ src/pages/ice_miyachi/taiseis_footprint.tsx
@@ -10,34 +10,26 @@ export default function TaiseisFootprint() {
       </Head>
 
       <div
-        style={{
-          width: "100vw",
-          height: "100dvh",
-          overflow: "visible",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "start",
-        }}
-      >
-        <div
-          style={{
-            transform: "scale(1)",
-            transformOrigin: "top center",
-            width: "100%",
-          }}
-        >
-          <iframe
-            src="https://dynamic-noodle-39f.notion.site/ebd//2b7c89ce71ed8093a67ed3df59c57eb5"
-            style={{
-              width: "100%",
-              height: "100%",
-              minHeight:"1500px",
-              flex: 1,
-              border: "none",
-              display: "block",
-            }}
-            scrolling="yes"
-          />
+  style={{
+    margin: 0,
+    padding: 0,
+    width: "100vw",
+    height: "100dvh",   // ← 画面全体の高さ
+    overflow: "hidden", // ← スクロールはiframeに任せる
+  }}
+>
+  <iframe
+    src="https://dynamic-noodle-39f.notion.site/2b7c89ce71ed8093a67ed3df59c57eb5?embed=true"
+    style={{
+      width: "100%",
+      height: "100%",    // ← 親要素に合わせて全画面
+      border: "none",
+      display: "block",
+    }}
+    scrolling="yes"
+  />
+</div>
+
         </div>
       </div>
     </>
