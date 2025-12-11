afb58fffec0d3bbc9f87e74c5301b01d10e3f8dd
ice-miyachi <icemiyachi@gmail.com>
2025-12-11 12:58:51 +0900

    taiseis_footprint.tsx を更新

--- src/pages/ice_miyachi/taiseis_footprint.tsx
+++ src/pages/ice_miyachi/taiseis_footprint.tsx
@@ -33,8 +33,7 @@ export default function TaiseisFootprint() {
           src="https://dynamic-noodle-39f.notion.site/ebd//2b7c89ce71ed8093a67ed3df59c57eb5"
           width="100%"
           height="100%"
-          border= "none"
-          display= "block"
+          style={{ border: "none", display: "block" }}
         />
       </div>
     </>
