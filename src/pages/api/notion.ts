adb1d232884ade55cc127dd4d2c8284363c879dd
ice-miyachi <icemiyachi@gmail.com>
2025-12-14 16:09:15 +0900

    notion.ts を更新

--- src/pages/api/notion.ts
+++ src/pages/api/notion.ts
@@ -1,8 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { Client } from "@notionhq/client";
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const databaseId = process.env.NOTION_DATABASE_ID!;
     const response = await notion.databases.query({
